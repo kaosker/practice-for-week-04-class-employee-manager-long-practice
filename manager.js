638c5fa773cbb7b4cbeeb38b10c35452e896853e
kaosker <thorup93@hotmail.com>
2023-05-20 18:20:43 +0200

    unfinished

--- manager.js
+++ manager.js
@@ -2,7 +2,7 @@ const Employee = require("./employee");
 
 class Manager extends Employee {
     constructor(name, salary, title, manager, employees = []) {
-       super(name, salary, title, manager, employees);
+       super(name, salary, title, manager);
        this.employees = employees;
     }
 
@@ -11,6 +11,18 @@ class Manager extends Employee {
     }
 }
 
-const splinter = new Manager('Splinter', 100000, 'Sensei');
 
-console.log(splinter instanceof Employee);
+
+
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+console.log('After: ', splinter);
+
+
+module.exports = Manager;
